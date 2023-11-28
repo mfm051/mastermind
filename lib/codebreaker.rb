6ee786d2f94ec0e8552f2913c14af3973e1d9668
mfm051 <mateusmatos051@gmail.com>
2023-11-28 11:04:32 -0300

    Add Codebreaker class
    Add simple method to guess from user input

---
+++ lib/codebreaker.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative 'pattern'
+
+# responsible from deciphering the code
+class Codebreaker
+  def initialize
+    @guess = nil
+  end
+
+  def guess_from_user
+    puts('Choose a four-letter sequence with letters between "A" and "H"')
+    user_guess = gets.chomp.upcase.delete(' ').split('')
+    @guess = Pattern.new(user_guess)
+  end
+end
